1318bbe9d8886689566e133671f7cd6b69a51c41
bz <62632238+BeeZLX@users.noreply.github.com>
2024-10-15 22:59:14 -0300

    Add files via upload

---
+++ aulalabirinto/aulalabirinto/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace aulalabirinto
+{
+    partial class labirinto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(labirinto));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pbx_avatar = new System.Windows.Forms.PictureBox();
+            this.pbx_Labirinto = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_avatar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Labirinto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pbx_avatar
+            // 
+            this.pbx_avatar.Image = global::aulalabirinto.Properties.Resources.direita;
+            resources.ApplyResources(this.pbx_avatar, "pbx_avatar");
+            this.pbx_avatar.Name = "pbx_avatar";
+            this.pbx_avatar.TabStop = false;
+            // 
+            // pbx_Labirinto
+            // 
+            this.pbx_Labirinto.Image = global::aulalabirinto.Properties.Resources.Labirinto;
+            resources.ApplyResources(this.pbx_Labirinto, "pbx_Labirinto");
+            this.pbx_Labirinto.Name = "pbx_Labirinto";
+            this.pbx_Labirinto.TabStop = false;
+            // 
+            // labirinto
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pbx_avatar);
+            this.Controls.Add(this.pbx_Labirinto);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "labirinto";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.labirinto_FormClosing);
+            this.Load += new System.EventHandler(this.labirinto_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.labirinto_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_avatar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Labirinto)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbx_Labirinto;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pbx_avatar;
+    }
+}
+
---
+++ aulalabirinto/aulalabirinto/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace aulalabirinto
+{
+    public partial class labirinto : Form
+    {
+        public labirinto()
+        {
+            InitializeComponent();
+        }
+        #region variaveis globais
+        static string[,] llabirinto = new string[24, 32] 
+        {
+            {"1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","0","0","1","1","0","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","0","0","1","1","0","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","1","0","1","1","0","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"0","0","0","1","1","0","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","1","0","1","1","0","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","1","0","1","1","0","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","1","0","1","1","0","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","1","0","1","1","0","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+            {"1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1",},
+        };
+        int posLinha=0, posColuna=12 ;
+        string controle;
+        #endregion
+
+        private void labirinto_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(MessageBox.Show("Deseja sair?", "Saindo...", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void labirinto_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar.ToString().ToUpper()=="W" || e.KeyChar.ToString().ToUpper() == "A" || e.KeyChar.ToString().ToUpper() == "S" || e.KeyChar.ToString().ToUpper() == "D")
+            {
+                controle = e.KeyChar.ToString().ToUpper();
+            }
+            else
+            {
+                e.KeyChar=Convert.ToChar(0);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int x=pbx_avatar.Location.X;
+            int y=pbx_avatar.Location.Y;
+
+            if((x==700) && (y == 472))
+            {
+                MessageBox.Show("Chegou na saida");
+            }
+            else if (controle == "W")
+            {
+                pbx_avatar.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\sobe.png");
+                pbx_avatar.Size = new Size(40, 49);
+
+                if (posLinha == 0) return;
+
+                if (llabirinto[posLinha - 1, posColuna] == "1")
+                {
+                    pbx_avatar.Location = new Point(x, y - 15);
+                    posLinha--;
+                }
+            }
+
+            else if (controle == "A")
+            {
+                pbx_avatar.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\esquerda.png");
+                pbx_avatar.Size = new Size(40, 49);
+
+                if (posColuna == 0) return;
+
+                if (llabirinto[posLinha, posColuna - 1] == "1")
+                {
+                    pbx_avatar.Location = new Point(x - 15, y);
+                    posColuna--;
+                }
+            }
+
+            else if(controle == "S")
+            {
+                pbx_avatar.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\desce.png");
+                pbx_avatar.Size = new Size(40, 49);
+
+                if(posLinha == 23) return;
+
+                if(llabirinto[posLinha +1, posColuna] == "1")
+                {
+                    pbx_avatar.Location = new Point(x, y + 15);
+                    posLinha++;
+                }
+            }
+
+            else if(controle == "D")
+            {
+                pbx_avatar.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\direita.png");
+                pbx_avatar.Size = new Size(40, 49);
+
+                if(posColuna == 31) return;
+
+                if (llabirinto[posLinha, posColuna + 1] == "1")
+                {
+                    pbx_avatar.Location = new Point(x + 15, y);
+                    posColuna++;
+                }
+            }
+        }
+
+        private void labirinto_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
